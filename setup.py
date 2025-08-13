a184a77ff71203a474e0670c29f507fba73ccd38
Kishore. V <157293196+vk22006@users.noreply.github.com>
2025-08-13 06:53:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2025 Kishore V
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2025 Kishore V
 
 from setuptools import setup, Extension
 import pybind11
